6cac2e2c5b13b55c944bba57f4452753cba02d5e
linhle <lklinh@outlook.com>
2020-08-28 10:53:16 +0700

    intro-part-finished

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+// document.getElementById("ll-rocketButton").onclick = function () {
+//   flyRocket();
+// };
+
+// function flyRocket() {
+//   //   document.getElementById("ll-rocketButton").style.paddingBottom = "100px";
+//   //   document.getElementById("ll-rocketButton").style.transform = "rotate(315deg)";
+//   document.getElementById("ll-rocketButton").style.animation = "fly-rocket 10s";
+// }
